311e04fa3806faa98a956285290270be3b85ba51
Dani <79950554+Kaserola4@users.noreply.github.com>
2025-11-07 19:20:53 -0600

    feat(backend): added test runner

--- backend/src/test/java/com/bookingmx/reservations/RunCucumberTest.java
+++ backend/src/test/java/com/bookingmx/reservations/RunCucumberTest.java
@@ -1,6 +1,21 @@
 package com.bookingmx.reservations;
 
 
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.FILTER_TAGS_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.bookingmx.reservations.steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports/cucumber.html, json:target/cucumber-reports/cucumber.json")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @Disabled")
 public class RunCucumberTest {
     // This class will remain empty, it's just a runner
 }
