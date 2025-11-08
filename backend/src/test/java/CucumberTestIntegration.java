ed1708a8797d56a73eaa0ddef10d48fde978d58d
Dani <79950554+Kaserola4@users.noreply.github.com>
2025-11-07 18:53:22 -0600

    chore(backend): add context configuration

---
+++ backend/src/test/java/CucumberTestIntegration.java
@@ -0,0 +1,8 @@
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@CucumberContextConfiguration
+class CucumberTestIntegration {
+    // executeGet implementation
+}
