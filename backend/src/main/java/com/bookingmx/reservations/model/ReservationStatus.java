8dee3db0c34027d6c08befd40acbbb55bd169e83
Dani <79950554+Kaserola4@users.noreply.github.com>
2025-11-08 18:41:31 -0600

    docs: add javadoc to Reservation enumaration

--- backend/src/main/java/com/bookingmx/reservations/model/ReservationStatus.java
+++ backend/src/main/java/com/bookingmx/reservations/model/ReservationStatus.java
@@ -1,5 +1,19 @@
 package com.bookingmx.reservations.model;
 
+/**
+ * Enumeration of possible reservation statuses.
+ * <p>
+ * A reservation can be in one of two states:
+ * <ul>
+ *   <li><b>ACTIVE</b> - The reservation is confirmed and active</li>
+ *   <li><b>CANCELED</b> - The reservation has been canceled</li>
+ * </ul>
+ * </p>
+ * 
+ * @author BookingMx Team
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public enum ReservationStatus {
     ACTIVE, CANCELED
 }
