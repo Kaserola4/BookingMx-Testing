774830c41471eee99405e194bc675aaae4f47220
Dani <79950554+Kaserola4@users.noreply.github.com>
2025-11-08 18:46:22 -0600

    docs: add javadoc to Bad request exception

--- backend/src/main/java/com/bookingmx/reservations/exception/BadRequestException.java
+++ backend/src/main/java/com/bookingmx/reservations/exception/BadRequestException.java
@@ -3,6 +3,18 @@ package com.bookingmx.reservations.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception thrown when a client request contains invalid data.
+ * <p>
+ * This exception maps to HTTP 400 Bad Request status and is used
+ * for business rule violations such as invalid dates or attempting
+ * to update canceled reservations.
+ * </p>
+ * 
+ * @author BookingMx Team
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
     public BadRequestException(String m) { super(m); }
